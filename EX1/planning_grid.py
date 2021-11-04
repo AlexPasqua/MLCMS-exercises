fa28c3dccffa88f6bdee928991ce069664a99b45
AlexPasqua <alexpasquali98@gmail.com>
2021-11-04 22:47:34 +0100

    small fix in documentation of planning_grid.py

--- EX1/planning_grid.py
+++ EX1/planning_grid.py
@@ -1,3 +1,6 @@
+from grid import *
+
+
 class PlanningGrid:
     """
     An object helping with the simulation handling, keeping it simpler to manage wrt using directly the graphical grid
@@ -16,26 +19,26 @@ class PlanningGrid:
         self.grid = []
         self.init_grid(cell_grid)
 
-    def init_grid(self, cell_grid):
+    def init_grid(self, cell_grid: CellGrid):
         """
         Construct a much simpler grid wrt the graphical purpose one.
         Planning grid is a matrix where each cell content is an integer identifying what is inside the cell
-        :param cell_grid:
-        :return:
+        :param cell_grid: the actual grid associated to the planning one
         """
         for row in range(len(cell_grid.grid)):
             grid_line = []
             for column in range(len(cell_grid.grid[row])):
+                # create a grid line where the cells are just numbers representing the cell's status
                 cell_status = cell_grid.grid[row][column].status
                 if cell_status == 'Blank':
                     grid_line.append(self.BLANK_CELL)
-                if cell_status == 'Person':
+                elif cell_status == 'Person':
                     grid_line.append(self.PEDESTRIAN_CELL)
                     self.pedestrian_list.append((row, column))  # to maintain a list of the pedestrian coordinates
-                if cell_status == 'Obstacle':
+                elif cell_status == 'Obstacle':
                     grid_line.append(self.OBSTACLE_CELL)
                     self.obstacles_list.append((row, column))  # to maintain a list of the obstacles coordinates
-                if cell_status == 'Target':
+                elif cell_status == 'Target':
                     grid_line.append(self.TARGET_CELL)
                     self.targets_list.append((row, column))  # to maintain a list of the targets coordinates
             self.grid.append(grid_line)
@@ -45,7 +48,6 @@ class PlanningGrid:
         Update a particular pedestrian position due to movement
         :param old_coord: past coordinates of the pedestrian, to be removed since the pedestrian is not there anymore
         :param new_coord: new coordinates of the pedestrian, to be added to the list
-        :return:
         """
         self.pedestrian_list.remove(old_coord)
         self.pedestrian_list.append(new_coord)
