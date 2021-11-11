32fec4b4c5329d89d4dc6f2dc879740723947d33
AlexPasqua <alexpasquali98@gmail.com>
2021-11-11 09:48:14 +0100

    Created functions to add pedestrians to scenarios in modify_scenario.py

--- EX2/task3/modify_scenario.py
+++ EX2/task3/modify_scenario.py
@@ -1,4 +1,5 @@
 import json
+import warnings
 
 
 def read_scenario(path="scenarios/rimea6.scenario"):
@@ -12,10 +13,118 @@ def read_scenario(path="scenarios/rimea6.scenario"):
         return scenario
 
 
-def add_pedestrian(scenario):
-    ped = {}
-    id = find_free_id(scenario)
-    print(id)
+def add_pedestrian(scenario=None, scenario_path=None, output_path=None, id=None, find_min_free_id=True, targetIds=[],
+                   radius=0.2, densityDependentSpeed=False, speedDistributionMean=1.34,
+                   speedDistributionStandardDeviation=0.26, minimumSpeed=0.5, maximumSpeed=2.2, acceleration=2.0,
+                   footstepHistorySize=4, searchRadius=1.0, walkingDirectionCalculation="BY_TARGET_CENTER",
+                   walkingDirectionSameIfAngleLessOrEqual=45.0, nextTargetListIndex=0, position=(0, 0), velocity=(0, 0),
+                   freeFlowSpeed=1.8522156059160915, followers=[], idAsTarget=-1, infectionStatus="SUSCEPTIBLE",
+                   lastInfectionStatusUpdateTime=-1.0, pathogenAbsorbedLoad=0.0, groupIds=[], groupSizes=[],
+                   agentsInGroup=[], traj_footsteps=[]):
+    # 'scenario' and 'scenario_path' cannot be both None
+    if scenario_path is None and scenario is None:
+        raise AttributeError("One of 'scenario' and 'scenario_path' must be not None, got both None")
+
+    # if the scenario path is not passed, than an output path is mandatory (otherwise the scenario path is used as output path too)
+    elif scenario_path is None and output_path is None:
+        raise AttributeError("One of 'scenario_path' and 'output_path' must be not None, got both None")
+
+    # if both the scenario and its path are passed, only the scenario is going to be used and the path is ignored
+    elif scenario_path is not None and scenario is not None:
+        msg = "Both the scenario and the path to its file were passed to the function 'add_pedestrian'. " \
+              "Only the scenario is going to be used, it will not be read again from file"
+        warnings.warn(msg, RuntimeWarning)
+
+    # if scenario_path is not None, read the scenario from file
+    if scenario_path is not None:
+        scenario = read_scenario(scenario_path)
+
+    # if the pedestrian's id is not passed, find a free one
+    if id is None:
+        id = find_free_id(scenario, find_min_free_id=True)
+
+    # create a dictionary with the pedestrian's data (in the format used in the scenario's json file)
+    ped = {
+        "attributes": {
+            "id": id,
+            "radius": radius,
+            "densityDependentSpeed": densityDependentSpeed,
+            "speedDistributionMean": speedDistributionMean,
+            "speedDistributionStandardDeviation": speedDistributionStandardDeviation,
+            "minimumSpeed": minimumSpeed,
+            "maximumSpeed": maximumSpeed,
+            "acceleration": acceleration,
+            "footstepHistorySize": footstepHistorySize,
+            "searchRadius": searchRadius,
+            "walkingDirectionCalculation": walkingDirectionCalculation,
+            "walkingDirectionSameIfAngleLessOrEqual": walkingDirectionSameIfAngleLessOrEqual
+        },
+        "source": None,
+        "targetIds": targetIds,
+        "nextTargetListIndex": nextTargetListIndex,
+        "isCurrentTargetAnAgent": False,
+        "position": {
+            "x": position[0],
+            "y": position[1]
+        },
+        "velocity": {
+            "x": velocity[0],
+            "y": velocity[1]
+        },
+        "freeFlowSpeed": freeFlowSpeed,
+        "followers": followers,
+        "idAsTarget": idAsTarget,
+        "isChild": False,
+        "isLikelyInjured": False,
+        "psychologyStatus": {
+            "mostImportantStimulus": None,
+            "threatMemory": {
+                "allThreats": [],
+                "latestThreatUnhandled": False
+            },
+            "selfCategory": "TARGET_ORIENTED",
+            "groupMembership": "OUT_GROUP",
+            "knowledgeBase": {
+                "knowledge": [],
+                "informationState": "NO_INFORMATION"
+            },
+            "perceivedStimuli": [],
+            "nextPerceivedStimuli": []
+        },
+        "healthStatus": {
+            "infectionStatus": infectionStatus,
+            "lastInfectionStatusUpdateTime": lastInfectionStatusUpdateTime,
+            "pathogenAbsorbedLoad": pathogenAbsorbedLoad,
+            "startBreatheOutPosition": None,
+            "respiratoryTimeOffset": -1.0,
+            "breathingIn": False,
+            "pathogenEmissionCapacity": -1.0,
+            "pathogenAbsorptionRate": -1.0,
+            "minInfectiousDose": -1.0,
+            "exposedPeriod": -1.0,
+            "infectiousPeriod": -1.0,
+            "recoveredPeriod": -1.0
+        },
+        "groupIds": groupIds,
+        "groupSizes": groupSizes,
+        "agentsInGroup": agentsInGroup,
+        "trajectory": {"footSteps": traj_footsteps},
+        "modelPedestrianMap": None,
+        "type": "PEDESTRIAN"
+    }
+
+    # "scenario['scenario']['topography']['dynamicElements']" gives the list of the pedestrians in the scenario;
+    # append to it the new pedestrian just created
+    scenario['scenario']['topography']['dynamicElements'].append(ped)
+
+    if output_path is None:  # if output_path is None, use scenario_path
+        output_path = scenario_path
+    elif not output_path.endswith(".scenario"):  # add ".scenario" suffix to the output path if not present
+        output_path += ".scenario"
+
+    # write the scenario file with the new pedestrian
+    with open(output_path, 'w') as f:
+        json.dump(scenario, f, indent='  ')
 
 
 def find_free_id(scenario: dict, find_min_free_id=True):
@@ -38,7 +147,7 @@ def find_free_id(scenario: dict, find_min_free_id=True):
         busy_ids.add(t['id'])
 
     if not find_min_free_id:
-        return max(busy_ids) + 1    # simply return the max busy id + 1 (which will be free)
+        return max(busy_ids) + 1  # simply return the max busy id + 1 (which will be free)
 
     # otherwise sort the busy ids and find the minimum free one
     sorted_ids = sorted(list(busy_ids))
@@ -56,5 +165,8 @@ def find_free_id(scenario: dict, find_min_free_id=True):
 
 
 if __name__ == '__main__':
-    scenario = read_scenario(path="../task1/scenarios/rimea1.scenario")
-    add_pedestrian(scenario)
+    add_pedestrian(
+        scenario_path="../task1/scenarios/rimea1.scenario",
+        output_path="scenarios/pippo.scenario",
+        position=(40, 40)
+    )
