dedc9b6989cc3064c168d2736af3539f697c7f0c
AlexPasqua <alexpasquali98@gmail.com>
2021-11-30 18:49:20 +0100

    Added plot_traj to task3.py

--- EX3/task3.py
+++ EX3/task3.py
@@ -2,6 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def plot_traj(start_pos, ts=1e-3, iter_num=10000):
+    """
+    Function to plot a trajectory for a certain amount of iterations, given a starting point.
+    @param start_pos: initial coordinates of point
+    @param ts: step length
+    @param iter_num: number of iterations to execute
+    """
+    X, Y = [], []  # lists to store positioning update
+    x = start_pos
+    for i in range(iter_num):
+        x1, x2 = x[0], x[1]
+        X.append(x1)
+        Y.append(x2)
+        # find vectors for next movement
+        u = alpha * x1 - x2 - x1 * (x1 ** 2 + x2 ** 2)
+        v = x1 + alpha * x2 - x2 * (x1 ** 2 + x2 ** 2)
+        # execute step
+        x = x + ts * np.array([u, v])
+    plt.figure(figsize=(10, 10))
+    # plot trajectory
+    plt.plot(X, Y)
+    plt.scatter(X[0], Y[0])  # put the starting point in evidence
+    plt.title(f"Trajectory [{start_pos[0]}, {start_pos[1]}]")
+    plt.xlim(-w, w)
+    plt.ylim(-w, w)
+    plt.show()
+
+
 if __name__ == '__main__':
     # TASK 3.1
     w = 3
