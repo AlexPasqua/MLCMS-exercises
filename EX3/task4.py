4a4a0a64dc331edda0eb02186a4b7598c6affc4b
AlexPasqua <alexpasquali98@gmail.com>
2021-11-30 19:50:29 +0100

    fatto task4 .1.2.3, da sistemare ma ci sta

--- EX3/task4.py
+++ EX3/task4.py
@@ -37,3 +37,56 @@ if __name__ == '__main__':
     plot_system(2.5, .1, 10, ax=ax1)
     plot_system(3.5, .1, 10, ax=ax2)
     plt.show()
+
+    # TASK 4.1
+    n = 5000
+    r = np.linspace(0, 2, n)
+    iterations = 1000
+    last = 100
+    x = 1e-5 * np.ones(n)
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 9), sharex=True)
+    for i in range(iterations):
+        x = logistic(r, x)
+        # We display the bifurcation diagram.
+        if i >= (iterations - last):
+            ax.plot(r, x, ',k', alpha=.25)
+    ax.set_title("Bifurcation diagram")
+    plt.tight_layout()
+    plt.show()
+
+    # TASK 4.2
+    n = 5000
+    r = np.linspace(2, 4, n)
+    iterations = 1000
+    last = 100
+    x = 1e-5 * np.ones(n)
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 9), sharex=True)
+    for i in range(iterations):
+        x = logistic(r, x)
+        # We display the bifurcation diagram.
+        if i >= (iterations - last):
+            ax.plot(r, x, ',k', alpha=.25)
+    # ax.set_xlim(2.5, 4)
+    ax.set_title("Bifurcation diagram")
+    plt.tight_layout()
+    plt.show()
+
+    # TASK 4.2
+    n = 5000
+    r = np.linspace(0, 4, n)
+    iterations = 1000
+    last = 100
+    x = 1e-5 * np.ones(n)
+
+    fig, ax = plt.subplots(1, 1, figsize=(8, 9), sharex=True)
+    for i in range(iterations):
+        x = logistic(r, x)
+        # We display the bifurcation diagram.
+        if i >= (iterations - last):
+            ax.plot(r, x, ',k', alpha=.25)
+    ax.set_ylim(0, 1)
+    ax.set_title("Bifurcation diagram")
+    plt.tight_layout()
+    plt.show()
