e54bd9f800b699846e0885cc0c80cc9bb1f1c778
AlexPasqua <alexpasquali98@gmail.com>
2021-12-03 17:52:26 +0100

    (read desc) On task4.py added phase portrait of lorenz attractor
    On task4.py added the code to generate the phase portrait of the Lorenz attractor.
    Of course task4.py has to be removed at a certain point, but for now I put the code there.
    Start from that code for the second part of the task 4, where we have to plot a single trajectory (instead of the phase portrait obtained by running the system many times)

--- EX3/task4.py
+++ EX3/task4.py
@@ -1,65 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from utilities import logistic, logistic_map_cobweb_plot
+from utilities import lorenz, logistic_map_cobweb_plot
 
 if __name__ == '__main__':
+    dt = 0.01
+    num_steps = 10000
+
+    # Need one more for the initial values
+    xs = np.empty(num_steps + 1)
+    ys = np.empty(num_steps + 1)
+    zs = np.empty(num_steps + 1)
+
+    # Set initial values
+    xs[0], ys[0], zs[0] = (0., 1., 1.05)
+
+    # Step through "time", calculating the partial derivatives at the current point
+    # and using them to estimate the next point
+    for i in range(num_steps):
+        x_dot, y_dot, z_dot = lorenz((xs[i], ys[i], zs[i]))
+        xs[i + 1] = xs[i] + (x_dot * dt)
+        ys[i + 1] = ys[i] + (y_dot * dt)
+        zs[i + 1] = zs[i] + (z_dot * dt)
+
+    # Plot
+    ax = plt.figure().add_subplot(projection='3d')
+
+    ax.plot(xs, ys, zs, lw=0.5)
+    ax.set_xlabel("X Axis")
+    ax.set_ylabel("Y Axis")
+    ax.set_zlabel("Z Axis")
+    ax.set_title("Lorenz Attractor")
+    plt.show()
+    exit()
+
     # TODO: decidi cosa fare con questa sezione -> è quella cosa che fa vedere i punti che saltano di qua e di là
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6), sharey=True)
     logistic_map_cobweb_plot(2.5, .1, 10, ax=ax1)
     logistic_map_cobweb_plot(3.5, .1, 10, ax=ax2)
-    plt.show()
-    exit()
-
-    # TASK 4.1
-    n = 5000
-    r = np.linspace(0, 2, n)
-    iterations = 1000
-    last = 100
-    x = 1e-5 * np.ones(n)
-
-    fig, ax = plt.subplots(1, 1, figsize=(8, 9), sharex=True)
-    for i in range(iterations):
-        x = logistic(r, x)
-        # We display the bifurcation diagram.
-        if i >= (iterations - last):
-            ax.plot(r, x, ',k', alpha=.25)
-    ax.set_title("Bifurcation diagram")
-    plt.tight_layout()
-    plt.show()
-
-    # TASK 4.2
-    n = 5000
-    r = np.linspace(2, 4, n)
-    iterations = 1000
-    last = 100
-    x = 1e-5 * np.ones(n)
-
-    fig, ax = plt.subplots(1, 1, figsize=(8, 9), sharex=True)
-    for i in range(iterations):
-        x = logistic(r, x)
-        # We display the bifurcation diagram.
-        if i >= (iterations - last):
-            ax.plot(r, x, ',k', alpha=.25)
-    # ax.set_xlim(2.5, 4)
-    ax.set_title("Bifurcation diagram")
-    plt.tight_layout()
-    plt.show()
-
-    # TASK 4.2
-    n = 5000
-    r = np.linspace(0, 4, n)
-    iterations = 1000
-    last = 100
-    x = 1e-5 * np.ones(n)
-
-    fig, ax = plt.subplots(1, 1, figsize=(8, 9), sharex=True)
-    for i in range(iterations):
-        x = logistic(r, x)
-        # We display the bifurcation diagram.
-        if i >= (iterations - last):
-            ax.plot(r, x, ',k', alpha=.25)
-    ax.set_ylim(0, 1)
-    ax.set_title("Bifurcation diagram")
-    plt.tight_layout()
     plt.show()
