d544e4636529430ddf7499bb841ea80bb3a9509d
AlexPasqua <alexpasquali98@gmail.com>
2021-12-29 18:47:10 +0100

    task1.1: improved func approx_lin_func

--- EX5/src/task1.py
+++ EX5/src/task1.py
@@ -1,35 +1,29 @@
 import pandas as pd
 import numpy as np
+from typing import Union, Iterable
 
 
-def approx_lin_func(data_path: str = None, coeffs: np.ndarray = None, targets: np.ndarray = None):
+def approx_lin_func(data: Union[str, Iterable[np.ndarray]]):
     """
     Approximate a linear function through least squares
-    :param data_path:
-        Path to the file containing the data in the format Nx2, col 0 is the data, col 1 the targets.
-        If None, then :param coeffs: and :param targets: must be not None.
-    :param coeffs:
-        Coefficients matrix.
-        If None, then :param data_path: must be not None and the value of :param targets: is ignored
-    :param targets:
-        Targets for each point in the coefficients matrix.
-        If None, then :param data_path: must be not None and the value of :param coeffs: is ignored
+    :param data:
+        Either str: path to the file containing the data in the format Nx2, col 0 is the data, col 1 the targets.
+        Or Iterable containing 2 numpy ndarrays:
+            1st: coefficients matrix
+            2nd: targets for each point in the coefficients matrix.
     :returns: tuple (least squares solution, residuals, rank of coefficients matrix, singular values of coefficient matrix)
     """
-    # check consistency of passed arguments
-    if data_path is None and (coeffs is None or targets is None):
-        raise AttributeError("If :param data_path: is None, then both :param coeffs: and :param targets: must be not None")
-    if data_path is not None and coeffs is not None and targets is not None:
-        data_path = None    # useful for later
-        raise Warning(":param coeffs: and :param targets: were passed, therefore the value of :param data_path: will be ignored")
-
-    # if data_path is not None, read the data, otherwise it means that the data is already in coeffs and targets due to the previous checks
-    if data_path is not None:
+    if isinstance(data, str):
+        data_path = data
         # read data
-        linear_func_data = pd.read_csv("../data/linear_function_data.txt", sep=" ", header=None, dtype=np.float64)
+        linear_func_data = pd.read_csv(data_path, sep=" ", header=None, dtype=np.float64)
         # divide data into auxiliary variables
         coeffs, targets = linear_func_data.iloc[:, 0], linear_func_data.iloc[:, 1]
         coeffs = np.expand_dims(coeffs, 1)  # add 1 dimension, needed for np.linalg.lstsq
+    else:
+        if len(data) != 2:
+            raise ValueError(f"Parameter data must be either a string or an Iterable of 2 numpy ndarrays, got {len(data)} elements")
+        coeffs, targets = data[0], data[1]
 
     # solve least square
     sol, residuals, rank, singvals = np.linalg.lstsq(a=coeffs, b=targets, rcond=None)
